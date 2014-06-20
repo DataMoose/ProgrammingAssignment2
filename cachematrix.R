eaca80d9a45f58506b403422f65a3a3ea8077638
Muhsin <muhsinkarim@outlook.com>
2014-06-20 20:20:24 +1000

    Submitting Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions will cache the inverse of a matrix to limit potential 
+## costly computation.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates a special "matrix" object that can 
+## cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+                
+                inv <- NULL                
 
+                set <- function(y) {
+                        x <<- y
+                        inv <<- NULL
+                }                
+                
+                get <- function() x
+                
+                setinverse <- function(solve) inv <<- solve  # Invert matrix x: solve(x)
+                
+                getinverse <- function() inv                
+                
+                # List containing functions
+                list(set = set, 
+                     get = get,
+                     setinverse = setinverse,
+                     getinverse = setinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has been 
+## calculated (and the matrix has not changed), the cachesolve 
+## retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+                inv <- x$getinverse()
+        
+                # Check if the inverted matrix inv is not null (not empty)
+                if(!is.null(inv)) {
+                        message("getting cached data")
+                        return(inv)  # Cached inverted matrix is returned
+                }
+                
+                data <- x$get()        
+
+                inv <- solve(data, ...)
+                
+                x$setinverse(inv)
+                
+                inv  # Inverted matrix                
 }
